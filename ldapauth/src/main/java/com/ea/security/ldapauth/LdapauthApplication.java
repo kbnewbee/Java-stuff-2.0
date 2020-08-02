b49eda7c64d30c27a3471abe19b5629a6afb83eb
Bairagi, Kallol <kbairagi@ea.com>
2020-08-02 23:09:45 +0530

    adding ldap auth

---
+++ ldapauth/src/main/java/com/ea/security/ldapauth/LdapauthApplication.java
@@ -0,0 +1,13 @@
+package com.ea.security.ldapauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdapauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdapauthApplication.class, args);
+	}
+
+}
---
+++ ldapauth/src/test/java/com/ea/security/ldapauth/LdapauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ea.security.ldapauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdapauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
