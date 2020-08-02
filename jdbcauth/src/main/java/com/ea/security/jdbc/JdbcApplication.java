34f7a80c6f4597b0bf8a132ebc8311922a0d4819
Bairagi, Kallol <kbairagi@ea.com>
2020-08-02 22:27:07 +0530

    adding jdbc auth

---
+++ jdbcauth/src/main/java/com/ea/security/jdbc/JdbcApplication.java
@@ -0,0 +1,15 @@
+package com.ea.security.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcApplication.class, args);
+
+    }
+
+
+}
